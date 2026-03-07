ac017c6cbf31d858fd908888fa13fddb3dd1a1bb
s76715939-glitch <s76715939@gmail.com>
2026-03-07 09:52:54 +0600

    login page done with login functionality

---
+++ assets/js/login.js
@@ -0,0 +1,15 @@
+const loginSubmit = () => {
+  const credentials = {
+    username: "admin",
+    password: "admin123",
+  };
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+  const error = document.querySelector("#error");
+  if (username === credentials.username && password === credentials.password) {
+    error.classList.add("hide");
+    window.location = "./home.html";
+  } else {
+    error.classList.remove("hide");
+  }
+};
