93e600dcd5a11905ea1a5e61fc6b00d9651e31bb
s76715939-glitch <s76715939@gmail.com>
2026-03-08 09:43:16 +0600

    all, open, close issue cards functionality done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,62 @@
 const fetchAllIssue = async () => {
-    const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
-    const res = await fetch(url);
-    const data = await res.json();
-    const array = data.data;
-    displayAllIssues(array);
-}
-
-// {
-//     "id": 1,
-//     "title": "Fix navigation menu on mobile devices",
-//     "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
-//     "status": "open",
-//     "labels": [
-//         "bug",
-//         "help wanted"
-//     ],
-//     "priority": "high",
-//     "author": "john_doe",
-//     "assignee": "jane_smith",
-//     "createdAt": "2024-01-15T10:30:00Z",
-//     "updatedAt": "2024-01-15T10:30:00Z"
-// }
+  showLoader();
+  const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+  const res = await fetch(url);
+  const data = await res.json();
+  const array = data.data;
+  displayIssues(array);
+  hideLoader();
+};
+const fetchOpenIssue = async () => {
+  showLoader();
+  const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+  const res = await fetch(url);
+  const data = await res.json();
+  const array = data.data.filter((item) => item.status === "open");
+  displayOpenIssue(array);
+  hideLoader();
+};
+const fetchClosedIssue = async () => {
+  showLoader();
+  const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+  const res = await fetch(url);
+  const data = await res.json();
+  const array = data.data.filter((item) => item.status === "closed");
+  displayClosedIssue(array);
+  hideLoader();
+};
 const getLabels = (labels) => {
-    const issueLabels = labels
-        .map((e) => `<div class="label label-bug">${e}</div>`)
-        .join("");
-    return issueLabels;
-}
-const displayAllIssues = (array) => {
-    const cardContainer = document.querySelector(".cards-container");
-    array.forEach(data => {
-        const html = `
-        <div class="card ${data.status=== "open"? "card-open": "card-close"}">
+  const issueLabels = labels
+    .map(
+      (e) =>
+        `<div class="label ${e === "bug" ? "label-bug" : e === "enhancement" ? "label-enhance" : "label-help"}">${e}</div>`,
+    )
+    .join("");
+  return issueLabels;
+};
+const clearActiveTabs = () => {
+  const allTabs = document.querySelectorAll(".tab-item");
+  allTabs.forEach((e) => {
+    e.classList.remove("btn-active");
+  });
+};
+const activeTab = (id) => {
+  const tab = document.querySelector(`#${id}`);
+  tab.classList.add("btn-active");
+};
+const displayIssues = (array) => {
+  clearActiveTabs();
+  activeTab("all");
+  const cardContainer = document.querySelector(".cards-container");
+  cardContainer.innerHTML = "";
+  const innerCounter = document.querySelector("#counter");
+  innerCounter.textContent = array.length;
+  array.forEach((data) => {
+    const html = `
+        <div class="card ${data.status === "open" ? "card-open" : "card-close"}">
         <div class="card-header">
-          <img src="./assets/${data.status=== "open"? "Open-Status.png": "Closed-Status.png"}" alt="" width="24px" height="24px" />
-          <div class="issue-level ${data.priority === "high" ? "high-issue" : data.priority === "medium" ? "medium-issue" :"low-issue"}">${data.priority}</div>
+          <img src="./assets/${data.status === "open" ? "Open-Status.png" : "Closed-Status.png"}" alt="" width="24px" height="24px" />
+          <div class="issue-level ${data.priority === "high" ? "high-issue" : data.priority === "medium" ? "medium-issue" : "low-issue"}">${data.priority}</div>
         </div>
         <div class="card-body">
           <h3 class="card-heading">${data.title}</h3>
@@ -50,8 +72,82 @@ const displayAllIssues = (array) => {
         </div>
       </div>
         `;
-        cardContainer.innerHTML += html;
-    });
+    cardContainer.innerHTML += html;
+  });
+};
+const displayOpenIssue = (array) => {
+  clearActiveTabs();
+  activeTab("open");
+  const cardContainer = document.querySelector(".cards-container");
+  cardContainer.innerHTML = "";
+  const innerCounter = document.querySelector("#counter");
+  innerCounter.textContent = array.length;
+  array.forEach((data) => {
+    const html = `
+        <div class="card ${data.status === "open" ? "card-open" : "card-close"}">
+        <div class="card-header">
+          <img src="./assets/${data.status === "open" ? "Open-Status.png" : "Closed-Status.png"}" alt="" width="24px" height="24px" />
+          <div class="issue-level ${data.priority === "high" ? "high-issue" : data.priority === "medium" ? "medium-issue" : "low-issue"}">${data.priority}</div>
+        </div>
+        <div class="card-body">
+          <h3 class="card-heading">${data.title}</h3>
+          <p class="card-paragraph">${data.description}</p>
+          <div class="issue-labels">
+            ${getLabels(data.labels)}
+          </div>
+        </div>
+        <div class="line"></div>
+        <div class="card-footer">
+          <p class="card-paragraph reporter">#1 by ${data.author}</p>
+          <p class="card-paragraph report-date">${new Date(data.createdAt).toLocaleDateString()}</p>
+        </div>
+      </div>
+        `;
+    cardContainer.innerHTML += html;
+  });
+};
+const displayClosedIssue = (array) => {
+  clearActiveTabs();
+  activeTab("closed");
+  const cardContainer = document.querySelector(".cards-container");
+  cardContainer.innerHTML = "";
+  const innerCounter = document.querySelector("#counter");
+  innerCounter.textContent = array.length;
+  array.forEach((data) => {
+    const html = `
+        <div class="card ${data.status === "open" ? "card-open" : "card-close"}">
+        <div class="card-header">
+          <img src="./assets/${data.status === "open" ? "Open-Status.png" : "Closed-Status.png"}" alt="" width="24px" height="24px" />
+          <div class="issue-level ${data.priority === "high" ? "high-issue" : data.priority === "medium" ? "medium-issue" : "low-issue"}">${data.priority}</div>
+        </div>
+        <div class="card-body">
+          <h3 class="card-heading">${data.title}</h3>
+          <p class="card-paragraph">${data.description}</p>
+          <div class="issue-labels">
+            ${getLabels(data.labels)}
+          </div>
+        </div>
+        <div class="line"></div>
+        <div class="card-footer">
+          <p class="card-paragraph reporter">#1 by ${data.author}</p>
+          <p class="card-paragraph report-date">${new Date(data.createdAt).toLocaleDateString()}</p>
+        </div>
+      </div>
+        `;
+    cardContainer.innerHTML += html;
+  });
+};
 
-}
+const showLoader = () => {
+  const loader = document.querySelector(".loader");
+  const cardContainer = document.querySelector(".cards-container");
+  cardContainer.classList.add("hide");
+  loader.classList.remove("hide");
+};
+const hideLoader = () => {
+  const loader = document.querySelector(".loader");
+  const cardContainer = document.querySelector(".cards-container");
+  cardContainer.classList.remove("hide");
+  loader.classList.add("hide");
+};
 fetchAllIssue();
