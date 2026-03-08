cdf55133d1a9b759d2f7add1ec8499fef4e93c03
s76715939-glitch <s76715939@gmail.com>
2026-03-08 10:25:07 +0600

    search issue functionality done

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,15 @@ const fetchClosedIssue = async () => {
   displayClosedIssue(array);
   hideLoader();
 };
+const fetchSearchIssue = async (query) => {
+  showLoader();
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${query}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  const array = data.data;
+  displaySearchIssue(array);
+  hideLoader();
+};
 const getLabels = (labels) => {
   const issueLabels = labels
     .map(
@@ -137,7 +146,36 @@ const displayClosedIssue = (array) => {
     cardContainer.innerHTML += html;
   });
 };
-
+const displaySearchIssue = (array) => {
+  clearActiveTabs();
+  const cardContainer = document.querySelector(".cards-container");
+  cardContainer.innerHTML = "";
+  const innerCounter = document.querySelector("#counter");
+  innerCounter.textContent = array.length;
+  array.forEach((data) => {
+    const html = `
+        <div class="card ${data.status === "open" ? "card-open" : "card-close"}">
+        <div class="card-header">
+          <img src="./assets/${data.status === "open" ? "Open-Status.png" : "Closed-Status.png"}" alt="" width="24px" height="24px" />
+          <div class="issue-level ${data.priority === "high" ? "high-issue" : data.priority === "medium" ? "medium-issue" : "low-issue"}">${data.priority}</div>
+        </div>
+        <div class="card-body">
+          <h3 class="card-heading">${data.title}</h3>
+          <p class="card-paragraph">${data.description}</p>
+          <div class="issue-labels">
+            ${getLabels(data.labels)}
+          </div>
+        </div>
+        <div class="line"></div>
+        <div class="card-footer">
+          <p class="card-paragraph reporter">#1 by ${data.author}</p>
+          <p class="card-paragraph report-date">${new Date(data.createdAt).toLocaleDateString()}</p>
+        </div>
+      </div>
+        `;
+    cardContainer.innerHTML += html;
+  });
+};
 const showLoader = () => {
   const loader = document.querySelector(".loader");
   const cardContainer = document.querySelector(".cards-container");
@@ -150,4 +188,9 @@ const hideLoader = () => {
   cardContainer.classList.remove("hide");
   loader.classList.add("hide");
 };
+const searchBtn = document.querySelector("#search-btn");
+searchBtn.addEventListener("click", () => {
+  const searchInput = document.querySelector("#search-input").value;
+  fetchSearchIssue(searchInput);
+});
 fetchAllIssue();
